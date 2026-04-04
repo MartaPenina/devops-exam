631544d1f4f4cd4934eb3ae9b7a3f8f088a0b32f
MartaPenina <penina.marta100605@gmail.com>
2026-04-04 15:31:17 +0300

    fix: update backend to use new endpoints syntax

--- task1/terraform/backend.tf
+++ task1/terraform/backend.tf
@@ -2,14 +2,17 @@
 
 terraform {
   backend "s3" {
-    endpoint = "https://fra1.digitaloceanspaces.com"
-    region   = "us-east-1"
-    bucket   = "penina-tfstate"
-    key      = "task1/terraform.tfstate"
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+    region = "us-east-1"
+    bucket = "penina-tfstate"
+    key    = "task1/terraform.tfstate"
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
     force_path_style            = true
   }
 }
